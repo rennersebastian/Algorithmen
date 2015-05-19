2ef2be6a06d112ec9b24072b1e67180d12c50a33
Sebastian Renner <renner.sebastian@gmail.com>
2015-05-19 18:04:10 +0200

    Mergesort implemented

--- src/de/htw/algo/hw4/HW4.java
+++ src/de/htw/algo/hw4/HW4.java
@@ -14,44 +14,61 @@ import javafx.stage.Stage;
 public class HW4 extends Application {
 	private Sorter sorter = new Sorter();
 	private static long startTime, durationIns, durationQuick, durationMerge;
-	private static final long MAX_N = 1000;
+	private static final int MAX_N = 2000;
+	private static final int STEPS = 100;
+	private static final int LOOPS = 500;
 	
-    @Override public void start(Stage stage) {   	   	
-        stage.setTitle("Sorting Algorithms");
-        //defining the axes
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override public void start(Stage stage) {   	
+    	/**
+    	 * Sorting Test
+    	 */
+    	List<Integer> testNums = generateRandomNumbers(25);
+    	printNumbers(testNums, "Input");
+    	printNumbers(sorter.quicksort(testNums), "Quicksort");
+    	printNumbers(sorter.insertionSort(testNums), "Insertionsort");
+    	printNumbers(sorter.mergeSort(testNums), "Mergesort");
+    	
+        stage.setTitle("Algorithmen - HW4");
         final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
         xAxis.setLabel("Input size n");
-        //creating the chart
         final LineChart<Number,Number> lineChart = 
                 new LineChart<Number,Number>(xAxis,yAxis);
                 
-        lineChart.setTitle("Sorting Algorithms");
-        
+        lineChart.setTitle("Sorting Algorithms");        
 
-        XYChart.Series quicksort = new XYChart.Series();
+        XYChart.Series quickSort = new XYChart.Series();
         XYChart.Series insertionSort = new XYChart.Series();
-        quicksort.setName("Quicksort");        
-        insertionSort.setName("Insertion Sort"); 
-        for(int i = 100; i <= MAX_N; i+=100){
+        XYChart.Series mergeSort = new XYChart.Series();
+        quickSort.setName("Quicksort");        
+        insertionSort.setName("Insertionsort"); 
+        mergeSort.setName("Mergesort"); 
+        for(int i = STEPS; i <= MAX_N; i+=STEPS){
         	durationIns = 0;
         	durationQuick = 0;
+        	durationMerge = 0;
         	List<Integer> rndNumbers = generateRandomNumbers(i);
-        	for(int j = 0; j < 500; j++){
+        	for(int j = 0; j < LOOPS; j++){
+	        	startTime = System.nanoTime();
+	        	sorter.insertionSort(rndNumbers);
+	    		durationIns += (System.nanoTime() - startTime);
 	        	startTime = System.nanoTime();
 	        	sorter.quicksort(rndNumbers);
 	    		durationQuick += (System.nanoTime() - startTime);
 	        	startTime = System.nanoTime();
-	        	sorter.insertionSort(rndNumbers);
-	    		durationIns += (System.nanoTime() - startTime);
+	        	sorter.mergeSort(rndNumbers);
+	    		durationMerge += (System.nanoTime() - startTime);
         	}
-    		quicksort.getData().add(new XYChart.Data<Integer, Long>(i, (durationQuick/500)/100000));
-    		insertionSort.getData().add(new XYChart.Data<Integer, Long>(i, (durationIns/500)/100000));
+        	quickSort.getData().add(new XYChart.Data<Integer, Long>(i, (durationQuick/LOOPS)));
+    		insertionSort.getData().add(new XYChart.Data<Integer, Long>(i, (durationIns/LOOPS)));
+    		mergeSort.getData().add(new XYChart.Data<Integer, Long>(i, (durationMerge/LOOPS)));
         }
 
         Scene scene  = new Scene(lineChart,800,600);
-        lineChart.getData().add(quicksort);
+        lineChart.getData().add(quickSort);
         lineChart.getData().add(insertionSort);
+        lineChart.getData().add(mergeSort);
        
         stage.setScene(scene);
         stage.show();
@@ -69,4 +86,14 @@ public class HW4 extends Application {
 	    }	
 	    return list;
 	}	
+    
+    public static void printNumbers(List<Integer> numbers, String type) {
+    	System.out.println("=====" + type + "=====");
+		System.out.println("Numbers:");
+		for (int i = 0; i < numbers.size()-1; ++i) {
+			System.out.print(numbers.get(i) + ", ");
+		}
+		System.out.print(numbers.get(numbers.size()-1));
+		System.out.println();		
+	}
 }
--- src/de/htw/algo/hw4/Sorter.java
+++ src/de/htw/algo/hw4/Sorter.java
@@ -1,51 +1,50 @@
 package de.htw.algo.hw4;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Sorter {
 	/**
 	 * QUICKSORT
 	 */
-	public List<Integer> quicksort(List<Integer> input){		
-		if(input.size() <= 1){
-			return input;
-		}		
-		int middle = (int) Math.ceil((double)input.size() / 2);
-		int pivot = input.get(middle); 
-		List<Integer> less = new ArrayList<Integer>();
-		List<Integer> greater = new ArrayList<Integer>();		
-		for (int i = 0; i < input.size(); i++) {
-			if(input.get(i) <= pivot){
-				if(i == middle){
-					continue;
+	public List<Integer> quicksort(List<Integer> input) {
+		if (!input.isEmpty()) {
+			Integer pivot = input.get(0); // This pivot can change to get faster results
+
+			List<Integer> less = new LinkedList<Integer>();
+			List<Integer> pivotList = new LinkedList<Integer>();
+			List<Integer> more = new LinkedList<Integer>();
+
+			// Partition
+			for (Integer i : input) {
+				if (i.compareTo(pivot) < 0) {
+					less.add(i);
 				}
-				less.add(input.get(i));
+				else if (i.compareTo(pivot) > 0)
+					more.add(i);
+				else
+					pivotList.add(i);
 			}
-			else{
-				greater.add(input.get(i));
-			}
-		}		
-		return concat(quicksort(less), pivot, quicksort(greater));
-	}
-	
-	public List<Integer> concat(List<Integer> less, int pivot, List<Integer> greater){		
-		List<Integer> list = new ArrayList<Integer>();		
-		for (int i = 0; i < less.size(); i++) {
-			list.add(less.get(i));
-		}		
-		list.add(pivot);		
-		for (int i = 0; i < greater.size(); i++) {
-			list.add(greater.get(i));
-		}		
-		return list;
-	}
+
+			// Recursively sort sublists
+			less = quicksort(less);
+			more = quicksort(more);
+
+			// Concatenate results
+			less.addAll(pivotList);
+			less.addAll(more);
+			return less;
+		}
+		return input;
+	}	
 	/**
-	 * INSERTION SORT
+	 * INSERTIONSORT
 	 */
-	public List<Integer> insertionSort(List<Integer> list) {
+	public List<Integer> insertionSort(List<Integer> input) {
 		List<Integer> result = new ArrayList<Integer>();
-		result.addAll(list);
+		result.addAll(input);
 		int size = result.size();
 		for (int i = 1; i < size; i++) {
 			Integer value = result.get(i);
@@ -56,6 +55,55 @@ public class Sorter {
 			}
 			result.set(j+1, value);
 		}		
+		return result;
+	}
+	/**
+	 * MERGESORT
+	 */
+	public List<Integer> mergeSort(List<Integer> input) {
+		if (input.size() <= 1)
+			return input;
+		int middle = input.size() / 2;
+		List<Integer> left = input.subList(0, middle);
+		List<Integer> right = input.subList(middle, input.size());
+		right = mergeSort(right);
+		left = mergeSort(left);
+		List<Integer> result = merge(left, right);
+		return result;
+	}
+	
+	private List<Integer> merge(List<Integer> left, List<Integer> right) {
+		List<Integer> result = new ArrayList<Integer>();
+		Iterator<Integer> it1 = left.iterator();
+		Iterator<Integer> it2 = right.iterator();
+
+		Integer x = it1.next();
+		Integer y = it2.next();
+		while (true) {
+			if (x.compareTo(y) <= 0) {
+				result.add(x);
+				if (it1.hasNext()) {
+					x = it1.next();
+				} else {
+					result.add(y);
+					while (it2.hasNext()) {
+						result.add(it2.next());
+					}
+					break;
+				}
+			} else {
+				result.add(y);
+				if (it2.hasNext()) {
+					y = it2.next();
+				} else {
+					result.add(x);
+					while (it1.hasNext()) {
+						result.add(it1.next());
+					}
+					break;
+				}
+			}
+		}
 		return result;
 	}
 }
