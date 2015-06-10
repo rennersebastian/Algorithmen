63c9c494c0ea32846346d2b61e54afd2e0274d1b
Sebastian Renner <renner.sebastian@gmail.com>
2015-06-10 15:30:50 +0200

    changed data structure from list to array

--- src/de/htw/algo/hw5/HW5.java
+++ src/de/htw/algo/hw5/HW5.java
@@ -1,93 +1,81 @@
 package de.htw.algo.hw5;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
-import de.htw.algo.hw4.Sorter;
-
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
-import javafx.stage.Stage;
-
-public class HW5 extends Application {
-	private static Sorter sorter = new Sorter();
+public class HW5{
 	private static Heap heap = new Heap();;
 	private static long startTime, durationQuick, durationHeap;
-	private static final int MAX_N = 500;
-	private static final int STEPS = 100;
+	private static final int MAX_N = 10000;
+	private static final int STEPS = 2000;
 	private static final int LOOPS = 100;
-	
-	@Override
-	public void start(Stage stage) { 
-		List<Integer> testNums = generateRandomNumbers(15);
-		
-    	printNumbers(testNums, "Input");
-    	printNumbers(sorter.quicksort(testNums), "Quicksort");
-    	printNumbers(heap.heapSort(testNums), "Heapsort");
-    	
-    	stage.setTitle("Algorithmen - HW5");
-        final NumberAxis xAxis = new NumberAxis();
-        final NumberAxis yAxis = new NumberAxis();
-        xAxis.setLabel("Input size n");
-        yAxis.setLabel("Time in ns");
-        final LineChart<Number,Number> lineChart = 
-                new LineChart<Number,Number>(xAxis,yAxis);
-                
-        lineChart.setTitle("Sorting Algorithms");     
-
-        XYChart.Series quickSort = new XYChart.Series();
-        XYChart.Series heapSort = new XYChart.Series();
-        quickSort.setName("Quicksort");
-        heapSort.setName("Heapsort");
-        
-        for(int i = STEPS; i <= MAX_N; i+=STEPS){
-        	durationQuick = 0;
-        	durationHeap = 0;
-        	List<Integer> input = generateRandomNumbers(i);
-        	
-        	for(int j = 0; j < LOOPS; j++){
-	        	startTime = System.nanoTime();
-	        	sorter.quicksort(input);
-	    		durationQuick += (System.nanoTime() - startTime);
-	        	startTime = System.nanoTime();
-	        	heap.heapSort(input);
-	    		durationHeap += (System.nanoTime() - startTime);
-        	}
-        	quickSort.getData().add(new XYChart.Data<Integer, Long>(i, (durationQuick/LOOPS)));
-        	heapSort.getData().add(new XYChart.Data<Integer, Long>(i, (durationHeap/LOOPS)));
-        }
-		
-        Scene scene  = new Scene(lineChart,800,600);
-        lineChart.getData().add(quickSort);
-        lineChart.getData().add(heapSort);
-       
-        stage.setScene(scene);
-        stage.show();
-	}
-
-	private static List<Integer> generateRandomNumbers(int n){		
-	    List<Integer> list = new ArrayList<Integer>(n);
-	    Random random = new Random();		
-	    for (int i = 0; i < n; i++) {
-		    list.add(random.nextInt(n * 10));
-	    }	
-	    return list;
-	}	
-	
-	public static void printNumbers(List<Integer> numbers, String type) {
-    	System.out.println("=====" + type + "=====");
-		for (int i = 0; i < numbers.size()-1; ++i) {
-			System.out.print(numbers.get(i) + ", ");
-		}
-		System.out.print(numbers.get(numbers.size()-1));
-		System.out.println();		
-	}
 
 	public static void main(String[] args) {
-        launch(args);
+		/**
+    	 * Sorting Test
+    	 */
+    	int[] testNums = generateRandomNumbers(25);
+    	int[] heapnums = testNums.clone();
+    	printNumbers(testNums, "Input");
+    	quickSort(testNums,0,testNums.length-1);
+    	printNumbers(testNums, "Quicksort");
+    	heap.sort(heapnums);
+    	printNumbers(heapnums, "HeapSort");
+    	System.out.println();
+        
+        for(int i = 10000; i <= MAX_N; i+=STEPS){
+        	durationQuick = 0;
+        	
+        	int[] input = generateRandomNumbers(i);
+        	int[] heapNums = input.clone();
+
+        	for(int j = 0; j < LOOPS; j++){
+	        	startTime = System.nanoTime();
+	        	quickSort(input, 0, input.length-1);
+	    		durationQuick += (System.nanoTime() - startTime);
+	        	startTime = System.nanoTime();
+	        	heap.sort(heapNums);
+	        	durationHeap += (System.nanoTime() - startTime);
+        	}
+        }
+        System.out.println((durationQuick/LOOPS)/10000 + " ms");
+        System.out.println((durationHeap/LOOPS)/10000 + " ms");
     }
+	
+	public static void quickSort(int[] A, int al, int ar){
+	    if (al<ar){
+	        int pivot = A[al], i=al, j=ar+1;
+	        while (true){
+	            while (A[++i]<pivot && i<ar){}
+	            while (A[--j]>pivot && j>al){}
+	            if (i<j) swap(A, i, j);
+	            else break;
+	        }
+	        swap(A, j, al);
+	        
+	        quickSort(A, al, j-1);
+	        quickSort(A, j+1, ar);
+	    }
+	}
+	public static void swap(int[] A, int i, int j){
+	    int t=A[i]; A[i]=A[j]; A[j]=t;
+	}
+
+	
+	private static int[] generateRandomNumbers(int n){		
+	    int[] rndNumbers = new int[n];
+	    Random random = new Random();		
+	    for (int i = 0; i < n; i++) {
+		    rndNumbers[i] = random.nextInt(n * 10);
+	    }	
+	    return rndNumbers;
+	}	
+	
+	public static void printNumbers(int[] numbers, String type) {
+    	System.out.println("=====" + type + "=====");
+		for (int i = 0; i < numbers.length-1; ++i) {
+			System.out.print(numbers[i] + ", ");
+		}
+		System.out.print(numbers[numbers.length-1]);
+		System.out.println();		
+	}
 }
--- src/de/htw/algo/hw5/Heap.java
+++ src/de/htw/algo/hw5/Heap.java
@@ -1,54 +1,49 @@
 package de.htw.algo.hw5;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Heap {
-	private static List<Integer> list = new ArrayList<Integer>();
-	private static int heapSize, maximum, left, right;
+	static int heapsize, maximum, left, right;
+	static int[] a;
 	
-	private static void swap(int i, int j){
-		int temp = list.get(i);
-		list.set(i, list.get(j));
-		list.set(j, temp);
-	}
+	public static void buildHeap(int []a){
+		heapsize=a.length-1;
+        for(int i=heapsize/2;i>=0;i--){
+        	maxHeapify(a,i);
+        }
+    }
 	
-	private static void maxHeapify(List<Integer> a, int i){
-		left = 2*i;
-		right = 2*i+1;
-		
-		if(left <= heapSize && a.get(left) > a.get(i))
-			maximum = left;
-		else
-			maximum = i;
-		
-		if(right <= heapSize && a.get(right) > a.get(maximum))
-			maximum = right;
-		
-		if(maximum != i){
-			swap(i, maximum);	
-			maxHeapify(a, maximum);
-		}
-			
-	}
+	public static void maxHeapify(int[] a, int i){ 
+        left=2*i;
+        right=2*i+1;
+        if(left <= heapsize && a[left] > a[i]){
+        	maximum=left;
+        }
+        else{
+        	maximum=i;
+        }
+        
+        if(right <= heapsize && a[right] > a[maximum]){
+        	maximum=right;
+        }
+        if(maximum!=i){
+        	swap(i,maximum);
+        	maxHeapify(a, maximum);
+        }
+    }
 	
-	private static void buildMaxHeap(List<Integer> a){
-		heapSize = a.size()-1;
-
-		for(int i = heapSize/2; i >= 0; i--)
-			maxHeapify(a, i);
-	}
+	public static void swap(int i, int j){
+        int t=a[i];
+        a[i]=a[j];
+        a[j]=t; 
+    }
 	
-	public List<Integer> heapSort(List<Integer> a){
-		list = a;
-		buildMaxHeap(list);
-		
-		for(int i = heapSize; i > 0; i--){
-			swap(0, i);
-			heapSize -= 1;
-			maxHeapify(list, 0);
-		}
-		
-		return list;
-	}
+	public void sort(int []a0){
+        a=a0;
+        buildHeap(a);
+        
+        for(int i=heapsize;i>0;i--){
+            swap(0, i);
+            heapsize=heapsize-1;
+            maxHeapify(a, 0);
+        }
+    }
 }
